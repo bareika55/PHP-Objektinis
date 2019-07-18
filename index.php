85e1fff7433f0e7c97e1c0c4dfd8066a903b7130
Kasparas <kasparas12@gmail.com>
2019-07-18 13:43:59 +0300

    File DB functions

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
 <?php
 
-class FileDB {
+class fileDB
+{
     private $file_name;
+    private $data;
 
-    function __construct($file_name)
+    public function load()
     {
-        $this->file_name = $file_name;
-    }
-}
+        if (file_exists($this->file_name)) {
+            $encoded_string = file_get_contents($this->file_name);
 
+            if ($encoded_string !== false) {
+                $this->data = json_decode($encoded_string, true);
+            }
+        }
+    }
+
+    public function getData($data)
+    {
+        if ($data == null) {
+            return $this->load();
+        } else {
+            return $data;
+        }
+    }
+    
+}
 
 ?>
