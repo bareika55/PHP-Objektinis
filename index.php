97ae4dc378efd3a9cc4211964f0437a4093b663f
Kasparas <kasparas12@gmail.com>
2019-07-19 12:20:58 +0300

    File DB /// TABLE

--- index.php
+++ index.php
@@ -4,18 +4,15 @@ class fileDB
 {
     private $file_name;
     private $data;
-
     public function load()
     {
         if (file_exists($this->file_name)) {
             $encoded_string = file_get_contents($this->file_name);
-
             if ($encoded_string !== false) {
                 $this->data = json_decode($encoded_string, true);
             }
         }
     }
-
     public function getData()
     {
         if ($this->data == null) {
@@ -23,17 +20,23 @@ class fileDB
         }
         return $this->data;
     }
-
     public function setData($data_array)
     {
         $this->data = $data_array;
     }
-
     public function save($data)
     {
         $encoded_json = json_encode($data);
         file_put_contents('data.txt', $encoded_json);
     }
-}
 
+    public function createTable ($table_name) {
+        $table = [];
+        if (!empty($table)) {
+            return false;
+        }
+        return true;
+    }
+}
+?>
 ?>
